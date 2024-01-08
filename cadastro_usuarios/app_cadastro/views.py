f0ba76757f1732391e30bdeb5fd26c95263c60cd
Matheus Granda <Matheus Granda>
2024-01-08 13:47:32 -0300

    Insert new delete buttons

--- cadastro_usuarios/app_cadastro/views.py
+++ cadastro_usuarios/app_cadastro/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
 
-# Create your views here.
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    novo_usuario = Usuario()
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    #Armazenar as informações no banco de dados.
-    novo_usuario.save() 
+    if request.method == 'POST':
+        novo_usuario = Usuario()
+        novo_usuario.nome = request.POST.get('nome')
+        novo_usuario.idade = request.POST.get('idade')
+        novo_usuario.save()
+
+    usuarios = {'usuarios': Usuario.objects.all()}
+    return render(request, 'usuarios/usuarios.html', usuarios)
+
+def deletar_usuario(request, usuario_id):
+    usuario = Usuario.objects.get(id_usuario=usuario_id)
     
-    #Exibir todos os usuários já cadastrados em uma nova página. 
-    usuarios = {
-        'usuarios': Usuario.objects.all()
-    }
-    # Retornar os dados para a página de listagem de usuários. 
-    return render(request,'usuarios/usuarios.html', usuarios) 
+    if request.method == 'POST':
+        usuario.delete()
+        return redirect('listagem_usuarios')  # Redirecionar para a página de usuários após a exclusão
     
- 
+    return render(request, 'usuarios/deletar_usuario.html', {'usuario': usuario})
--- cadastro_usuarios/cadastro_usuarios/urls.py
+++ cadastro_usuarios/cadastro_usuarios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # rota, view responsável e nome de referência. 
     path('',views.home,name='home'), 
     path('usuarios/',views.usuarios,name='listagem_usuarios'), 
+    path('deletar_usuario/<int:usuario_id>/', views.deletar_usuario, name='deletar_usuario'),
+
 ]
