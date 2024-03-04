4cc78eeea3b17aa36f6976d1fb88e8aefa85511f
Matheus Granda <Matheus Granda>
2024-03-04 17:03:17 -0300

    Insert new tags

--- app_cadastro/models.py
+++ app_cadastro/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
-    idade = models.IntegerField()
+    produto = models.TextField(max_length=255, default='Sem produto')
+
+    def __str__(self):
+        return self.nome
--- app_cadastro/views.py
+++ app_cadastro/views.py
@@ -1,35 +1,43 @@
 from django.shortcuts import render, redirect
 from .models import Usuario
+from decimal import Decimal
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
+    valor_digitado = request.POST.get('valor_digitado')  # Acessar valor digitado
+
     if request.method == 'POST':
         novo_usuario = Usuario()
         novo_usuario.nome = request.POST.get('nome')
-        novo_usuario.idade = request.POST.get('idade')
+        novo_usuario.produto = request.POST.get('produto')
+
+        # Substituir vírgulas por pontos no valor e converter para Decimal
+        valor = request.POST.get('valor').replace(',', '.')
+        novo_usuario.valor = Decimal(valor)
+
         novo_usuario.save()
 
-    usuarios = {'usuarios': Usuario.objects.all()}
-    return render(request, 'usuarios/usuarios.html', usuarios)
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/usuarios.html', {'usuarios': usuarios, 'valor_digitado': valor_digitado})
 
 def deletar_usuario(request, usuario_id):
     usuario = Usuario.objects.get(id_usuario=usuario_id)
-    
+
     if request.method == 'POST':
         usuario.delete()
-        return redirect('listagem_usuarios')  # Redirecionar para a página de usuários após a exclusão
-    
+        return redirect('listagem_usuarios')  # Redireciona após a exclusão
+
     return render(request, 'usuarios/deletar_usuario.html', {'usuario': usuario})
 
 def editar_usuario(request, usuario_id):
     usuario = Usuario.objects.get(id_usuario=usuario_id)
 
     if request.method == 'POST':
-        usuario.nome = request.POST.get('nome')
-        usuario.idade = request.POST.get('idade')
+        usuario.produto = request.POST.get('produto')
+        usuario.valor = request.POST.get('valor')  # Atualiza o valor do produto
         usuario.save()
-        return redirect('listagem_usuarios')  # Redirecionar para a página de usuários após a edição
+        return redirect('listagem_usuarios')  # Redireciona após a edição
 
     return render(request, 'usuarios/editar_usuario.html', {'usuario': usuario})
