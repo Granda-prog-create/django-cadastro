81585aa58dd8fdf39e98a85ed89ccfa0bbf01d31
Matheus Granda Cesar <79712782+Granda-prog-create@users.noreply.github.com>
2024-03-05 13:33:34 -0300

    Delete cadastro_usuarios directory

--- cadastro_usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app_cadastro import views
-
-urlpatterns = [
-    path('', views.home, name='home'), 
-    path('usuarios/', views.usuarios, name='listagem_usuarios'), 
-    path('deletar_usuario/<int:usuario_id>/', views.deletar_usuario, name='deletar_usuario'),
-    path('editar_usuario/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),  
-]
